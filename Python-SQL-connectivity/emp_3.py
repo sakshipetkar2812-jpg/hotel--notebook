0132b450a729d0386b1ed4226e0e5f95a1633c8a
sakshipetkar2812-jpg <sakshipetkar2812@gmail.com>
2026-02-09 19:51:49 +0530

    Update emp_3.py

--- Python-SQL-connectivity/emp_3.py
+++ Python-SQL-connectivity/emp_3.py
@@ -5,7 +5,7 @@ import numpy as np
 conn= mysql.connector.connect(
     host="localhost",
     user="root",
-    password="7984455774sak",
+    password="",
     database="employee"
 )
 cursor=conn.cursor()
@@ -31,3 +31,4 @@ cursor.close()
 conn.close()
 print("All tasks completed")
  
+
