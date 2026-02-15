e2fb6f0a1a809df34da8b4bb6123bb76b3e76479
sakshipetkar2812-jpg <sakshipetkar2812@gmail.com>
2026-02-15 22:28:11 +0530

    Add files via upload

---
+++ Python-practice/Basic/Day 2.py
@@ -0,0 +1,19 @@
+name="SAKSHI"
+age=22
+weight=55.5
+goal="DATA SCITINIST"
+
+print("My name is",name)
+print("My age is",age)
+print("My body weight is",weight)
+print("My goal is to become",goal)
+
+var=True
+print(type(var))
+
+var_1=""
+print(type(var_1))
+ 
+var_2="1"+"0"
+print(var_2)
+print(type(var_2))
